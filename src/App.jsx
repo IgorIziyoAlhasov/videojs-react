d456e2dab809b36d4b0106f138eca9e1de059795
IgorIziyoAlhasov <igor.akira.yuki@gmail.com>
2023-12-05 00:08:38 +0200

    navigation added and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import { ApplicationNav, Footer } from './components'
-import './main.scss'
-import { Basic, CustomControls, Home, Playlist, Redesigned } from './pages'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { ApplicationNav, Footer } from './components';
+import './main.scss';
+import { Basic, CustomControls, Home, Playlist, Redesigned } from './pages';
 
 const App = () => {
   return (
@@ -10,16 +9,16 @@ const App = () => {
       <Router>
         <ApplicationNav />
         <Routes>
-          <Route path='' element={<Home />} />
-          <Route path='/basic' element={<Basic />} />
-          <Route path='/custom' element={<CustomControls />} />
-          <Route path='/playlist' element={<Playlist />} />
-          <Route path='/redesign' element={<Redesigned />} />
+          <Route path="" element={<Home />} />
+          <Route path="/basic" element={<Basic />} />
+          <Route path="/custom" element={<CustomControls />} />
+          <Route path="/playlist" element={<Playlist />} />
+          <Route path="/redesign" element={<Redesigned />} />
         </Routes>
         <Footer />
       </Router>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/ApplicationNav.jsx
+++ src/components/ApplicationNav.jsx
@@ -1,18 +1,102 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import './styles/application-nav.scss';
 
 const ApplicationNav = () => {
   return (
-    <header className='app-header'>
-      <nav className='app-navigation'>
-        <NavLink to="/" className={({ isActive }) => isActive ? 'menu-item-active' : ''}>Home</NavLink>
-        <NavLink to="/basic" className={({ isActive }) => isActive ? 'menu-item-active' : ''}>Basic</NavLink>
-        <NavLink to="/custom" className={({ isActive }) => isActive ? 'menu-item-active' : ''}>Custom</NavLink>
-        <NavLink to="/playlist" className={({ isActive }) => isActive ? 'menu-item-active' : ''}>Playlist</NavLink>
-        <NavLink to="/redesign" className={({ isActive }) => isActive ? 'menu-item-active' : ''}>Redesigned</NavLink>
+    <header className="app-header">
+      <nav className="app-navigation">
+        <div className="wrapper">
+          <ul className="nav-links">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? 'menu-item-active' : ''
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/basic"
+                className={({ isActive }) =>
+                  isActive ? 'menu-item-active' : ''
+                }
+              >
+                Basic
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/custom"
+                className={({ isActive }) =>
+                  isActive ? 'menu-item-active' : ''
+                }
+              >
+                Custom
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/playlist"
+                className={({ isActive }) =>
+                  isActive ? 'menu-item-active' : ''
+                }
+              >
+                Playlist
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/redesign"
+                className={({ isActive }) =>
+                  isActive ? 'menu-item-active' : ''
+                }
+              >
+                Redesigned
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        {/* <div className="wrappper">
+          <div className="nav-links">
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? 'menu-item-active' : '')}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/basic"
+              className={({ isActive }) => (isActive ? 'menu-item-active' : '')}
+            >
+              Basic
+            </NavLink>
+            <NavLink
+              to="/custom"
+              className={({ isActive }) => (isActive ? 'menu-item-active' : '')}
+            >
+              Custom
+            </NavLink>
+            <NavLink
+              to="/playlist"
+              className={({ isActive }) => (isActive ? 'menu-item-active' : '')}
+            >
+              Playlist
+            </NavLink>
+            <NavLink
+              to="/redesign"
+              className={({ isActive }) => (isActive ? 'menu-item-active' : '')}
+            >
+              Redesigned
+            </NavLink>
+          </div>
+        </div> */}
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default ApplicationNav
+export default ApplicationNav;
