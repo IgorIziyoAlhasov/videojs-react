9842664f8e76648ac22235ed09394b2dcf637fd6
IgorIziyoAlhasov <igor.akira.yuki@gmail.com>
2023-12-17 12:36:40 +0200

    made some eye-catching colors and menu items order swap

--- src/pages/Redesigned.jsx
+++ src/pages/Redesigned.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Player from '../components/Player'
+import './../styles/Redesigned.scss'
 
 const Redesigned = () => {
+  const [currentVideo, setCurrentVideo] = useState('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4')
+
+  const videoJsOptions = {
+    autoplay: false,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src: currentVideo,
+      type: 'video/mp4'
+    }]
+  };
   return (
-    <div>Redesigned Implementation</div>
+    <section className='page-content'>
+      <h1 className='section-title'>VideoJS - Custom Design</h1>
+      <p className='section-content'>Here is a player with some customizations implementation</p>
+
+      <div className="media-container">
+        <div className="vjs-container custom-style">
+          <Player
+            options={videoJsOptions}
+            currentVideo={currentVideo}
+          />
+        </div>        
+      </div>
+
+    </section>
   )
 }
 
