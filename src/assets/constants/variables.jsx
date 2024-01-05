34496220ed18f36ffb9604515807815ee2baac39
IgorIziyoAlhasov <igor.akira.yuki@gmail.com>
2024-01-05 14:26:59 +0200

    playlist-before-final-touches

---
+++ src/assets/constants/variables.jsx
@@ -0,0 +1,41 @@
+const playlistAPI = [
+    {
+        videoTitle: `Video #1. The first Blender Open Movie from 2006`,
+        src: `http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4`,
+        type: 'video/mp4'
+    },
+    {
+        videoTitle: `Video #2. HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV`,
+        src: `http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4`,
+        type: 'video/mp4'
+    },
+    {
+        videoTitle: `Video #3. The easiest way to enjoy online video and music on your TV—for when Batman's escapes aren't quite big enough`,
+        src: `http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4`,
+        type: 'video/mp4'
+    },
+    {
+        videoTitle: `Video #4. Sintel is an independently produced short film, initiated by the Blender Foundation.`,
+        src: `http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4`,
+        type: 'video/mp4'
+    },
+    {
+        videoTitle: `Video #5. Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers`,
+        src: `http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4`,
+        type: 'video/mp4'
+    },
+    {
+        videoTitle: `Video #6. Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender`,
+        src: `http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4`,
+        type: 'video/mp4'
+    },
+    {   
+        videoTitle: `Video #7. Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself`,
+        src: `http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4`,
+        type: 'video/mp4'
+    }
+];
+
+export {
+    playlistAPI
+}
--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import Player from '../components/Player';
+import VJSPlaylist from '../components/VJSPlaylist';
+import '../styles/basic.scss';
 
 const Playlist = () => {
+  const sourcePickerRef = useRef(null);
+  const [currentVideo, setCurrentVideo] = useState('src/assets/vids/small_vid.mp4')
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src: currentVideo,
+      type: 'video/mp4'
+    }]
+  };
+
+  const copyToClipBoard = (e) => {
+    let targetElement = e.target;
+
+    navigator.clipboard.writeText(targetElement.innerText)
+      .then(() => {
+        targetElement.classList.add('copied');
+        setTimeout(() => {
+          targetElement.classList.remove('copied');
+        }, 1000);
+      });
+  }
+
+  const handleVideoChangeClick = (e) => {
+    setCurrentVideo(sourcePickerRef.current.value);
+  }
   return (
-    <div>Playlist</div>
+    <section className='page-content'>
+      <h1 className='section-title'>Playlist Implementation</h1>
+      <p className='section-content'>Here is my take on playlist for Video.js</p>
+
+      <div className="media-container">
+        <div className="vjs-container">
+          <Player
+            options={videoJsOptions}
+          />
+        </div>
+        <div className="player-input">
+          <p className='section-descriptions'>Video playlist</p>
+          <div className='source-picker-container'>
+            <p htmlFor="video-source-picker">Pick video:</p>
+            <VJSPlaylist setVideo={setCurrentVideo}/>
+          </div>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
